e8cb8ae501c53e680e73f54e6bf3e7c6b9a8dddf
gukazma <gukazma@proton.me>
2024-10-31 10:24:29 +0800

    ✨ feat: 添加三维窗口

--- Apps/Qt5DemoApp/MainWindow.cpp
+++ Apps/Qt5DemoApp/MainWindow.cpp
@@ -2,12 +2,49 @@
 #include "ui_MainWindow.h"
 #include <QApplication>
 #include <QDebug>
+#include <Qt3DExtras>
+#include <Qt3DCore>
+#include <Qt3DRender>
+#include <Qt3DInput>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // 创建Qt3D视图
+    Qt3DExtras::Qt3DWindow* view = new Qt3DExtras::Qt3DWindow();
+    QWidget* container = QWidget::createWindowContainer(view);
+    container->setMinimumSize(200, 200); // 设置最小大小
+    // 创建一个3D场景
+    Qt3DCore::QEntity* rootEntity = new Qt3DCore::QEntity();
 
+    // 创建一个球体
+    Qt3DExtras::QSphereMesh* sphereMesh = new Qt3DExtras::QSphereMesh();
+    sphereMesh->setRadius(1);
+
+    // 创建一个材质
+    Qt3DExtras::QPhongMaterial* material = new Qt3DExtras::QPhongMaterial();
+    material->setDiffuse(QColor(QRgb(0x00ff00)));
+
+    // 创建一个变换组件
+    Qt3DCore::QTransform* transform = new Qt3DCore::QTransform();
+    transform->setTranslation(QVector3D(0, 0, 0));
+
+    // 将球体、材质和变换组件添加到一个实体中
+    Qt3DCore::QEntity* sphereEntity = new Qt3DCore::QEntity(rootEntity);
+    sphereEntity->addComponent(sphereMesh);
+    sphereEntity->addComponent(material);
+    sphereEntity->addComponent(transform);
+
+    // 创建一个摄像机
+    Qt3DRender::QCamera* camera = view->camera();
+    camera->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f);
+    camera->setPosition(QVector3D(0, 0, 10));
+    camera->setViewCenter(QVector3D(0, 0, 0));
+
+    // 将场景设置为视图的根实体
+    view->setRootEntity(rootEntity);
+    ui->groupBox_view->layout()->addWidget(container);
     QList<QSerialPortInfo> serialPortInfos = QSerialPortInfo::availablePorts();
     qDebug() << "Available ports:";
     for (const QSerialPortInfo& info : serialPortInfos) {
@@ -20,10 +57,16 @@ MainWindow::MainWindow(QWidget *parent) :
         serial.close();
         QString  port = ui->comboBox->currentText();
         int frequency = ui->spinBox->value();
+        qDebug() << "frequency: " << frequency;
+        qDebug() << "port: " << port;
         serial.setPortName(port); // 替换为你的串口号
         serial.setBaudRate(frequency);
         if (!serial.open(QIODevice::ReadOnly)) {
             qDebug() << "Failed to open serial port";
+        }
+        else
+        {
+            qDebug() << "Success to open serial port";
         }
       });
     connect(&timer, &QTimer::timeout, [&]() {
